3dc712ac11d4c08a1cf4639ba62a58ef1a5c43fe
范志俊 <fanfzj@163.com>
2014-03-29 19:27:35 +0800

    delete

--- ktv/Program.cs
+++ ktv/Program.cs
@@ -16,7 +16,7 @@ namespace ktv
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new login());
         }
     }
 }
